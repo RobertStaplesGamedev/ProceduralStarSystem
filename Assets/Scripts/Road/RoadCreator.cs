dd12a1c377cf709aeaa88128d3ea3aefeb3be14c
robstapleskablamo <rob.staples@assemblypayments.com>
2018-10-25 20:00:26 +1100

    changing code

---
+++ Assets/Scripts/Road/RoadCreator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadCreator : MonoBehaviour {
+
+	[HideInInspector]
+	public Road road;
+
+	public enum Orientation {Horizontal, Vertical};
+
+	public Orientation orientation;
+
+	public Color anchorCol = Color.red;
+	public Color controlCol = Color.white;
+	public Color segmentCol = Color.green;
+	public Color selectedSegmentCol = Color.yellow;
+	public float anchorDiameter = .1f;
+	public float controlDiameter = 0.075f;
+	[HideInInspector]
+	public bool displayControlPoints = true;
+
+	public void CreateRoad() {
+		road = new Road(transform.position);
+	}
+
+	void Reset() {
+		CreateRoad();
+	}
+}
